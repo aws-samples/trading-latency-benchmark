fb3f77ce7061a1445711066f1c8e39d0a85118a9
Shing Lyu <shinglyu@amazon.nl>
2023-10-24 16:17:57 +0200

    feat: static websocket endpoints

--- mock-trading-server/src/main.rs
+++ mock-trading-server/src/main.rs
@@ -1,23 +1,30 @@
-use actix_web::{App, HttpServer, Responder, post, HttpResponse};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, get, post, HttpResponse, Error};
+use actix_web_actors::ws;
 
-// Create a POST endpoint that has the following spec:
-// addBalances Request: 
-//http://localhost:8888/private/account/user/balances/3002/USDT/100000000 POST
-//addBalances Response:
-//User Created and balances sent for user:3002
-//OK: 200
-
+mod websocket;
+use self::websocket::WebSocketActor;
 
 #[post("/private/account/user/balances/{user_id}/{currency}/{amount}")]
 async fn add_balances(path: actix_web::web::Path<(i32, String, i32)>) -> impl Responder {
-    let (user_id, currency, amount) = path.into_inner();
+    let (user_id, _currency, _amount) = path.into_inner();
     HttpResponse::Ok().body(format!("User Created and balances sent for user: {}", user_id))
 }
+
+#[get("/")]
+async fn ws_index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let resp = ws::start(WebSocketActor::new(), &req, stream);
+    println!("{:?}", resp);
+    resp
+}
+
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(add_balances)
+        App::new()
+        .service(add_balances)
+        .service(ws_index)
     })
         .bind(("127.0.0.1", 8888))?
         .run()
