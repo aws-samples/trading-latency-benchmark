83f70ff0f12530899400475d594106f3f1b8e553
Shing Lyu <shinglyu@amazon.nl>
2023-10-24 16:45:40 +0200

    feat: dynamic subscriptions

--- mock-trading-server/src/websocket.rs
+++ mock-trading-server/src/websocket.rs
@@ -47,6 +47,17 @@ struct AuthRequest {
     api_token: String,
 }
 
+#[derive(Deserialize)]
+struct Channel {
+    name: String,
+}
+
+#[derive(Deserialize)]
+struct SubscriptionRequest {
+    r#type: String,
+    channels: Vec<Channel>,
+}
+
     
 
 pub struct WebSocketActor {
@@ -74,35 +85,6 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebSocketActor {
                 let payload: Value = serde_json::from_str(&text).unwrap(); 
                 let payload_type = payload["type"].as_str().unwrap();
 
-                //Handle the following message types
-                //Auth Request: 
-                //
-                //{"type":"AUTHENTICATE","api_token":"3002"}
-                //
-                //✓ Auth Acked Response:
-                //
-                //{type=AUTHENTICATED}
-                //
-                //Subscription Request:
-                //
-                //{"type":"SUBSCRIBE","channels":[{"name":"ORDERS"}]}
-                //
-                //✓ Subscriptions Acked
-                //
-                //{"channels":[{"account_id":3002,"name":"TRADING"}],"type":"SUBSCRIPTIONS","time":1698064922324}
-                //
-                //Limit Order Request:
-                //
-                //{"type":"CREATE_ORDER","order":{"instrument_code":"BTC_USDC","client_id":"4df7275e-6af9-4f2a-b620-7a9f6527a4f0","side":"BUY","type":"LIMIT","price":"1","amount":"1","time_in_force":"GOOD_TILL_CANCELLED"}}
-                //
-                //✓ Limit Order Acked:
-                //
-                //{"order_book_sequence":25532628695,"side":"SELL","uid":3002,"amount":"1","price":"1","instrument_code":"BTC_USDC","client_id":"4df7275e-6af9-4f2a-b620-7a9f6527a4f0","order_id":"2e81f80a-d6a0-48c0-bf17-4f63ba01d5a1","channel_name":"TRADING","type":"BOOKED","time":2002205959737}
-                //
-                //Cancel Order Request: 
-                //
-                //{"type":"CANCEL_ORDER","client_id":"4df7275e-6af9-4f2a-b620-7a9f6527a4f0","instrument_code":"BTC_USDC"}
-
                 match payload_type {
                     "AUTHENTICATE" => {
                         // parse the json into AuthRequest
@@ -113,13 +95,20 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebSocketActor {
                     },
                     "SUBSCRIBE" => {
                         let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis();
+                        let subscription_request: SubscriptionRequest = serde_json::from_str(&text).unwrap();
+
+                        // convert the subscription_reqeust.channels to the response channels format, add account_id to each json object in the array
+
+                        let output_channels = subscription_request.channels.iter().map(|channel| {
+                            json!({
+                                "account_id": self.user_id.as_ref().unwrap(),
+                                "name": channel.name
+                            })
+                        }).collect::<Vec<Value>>();
+
                         ctx.text(json!({
                             "type": "SUBSCRIPTIONS",
-                            "channels": [{ // TODO: make this dynamic
-                                "account_id": self.user_id.as_ref().unwrap(),
-                                "name": "TRADING"
-                            }],
-                            //return the current epoch
+                            "channels": output_channels,
                             "time": timestamp,
                         }).to_string());
                     },
