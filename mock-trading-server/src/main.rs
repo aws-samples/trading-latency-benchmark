d96d1ed84db8aae2e0775236f7b064834bf00c80
Shing Lyu <shinglyu@amazon.nl>
2023-10-25 10:17:55 +0200

    feat: added env logger

--- mock-trading-server/src/main.rs
+++ mock-trading-server/src/main.rs
@@ -1,33 +1,42 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder, get, post, HttpResponse, Error};
+use actix_web::middleware::Logger;
 use actix_web_actors::ws;
+
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 
 mod websocket;
 use self::websocket::WebSocketActor;
 
 #[post("/private/account/user/balances/{user_id}/{currency}/{amount}")]
 async fn add_balances(path: actix_web::web::Path<(i32, String, i32)>) -> impl Responder {
-    let (user_id, _currency, _amount) = path.into_inner();
+    let (user_id, currency, amount) = path.into_inner();
+    info!("Adding balances for user {}, currency {}, amount {}", user_id, currency, amount);
     HttpResponse::Ok().body(format!("User Created and balances sent for user: {}", user_id))
 }
 
 #[get("/")]
 async fn ws_index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    info!("Websocket connection received");
     let resp = ws::start(WebSocketActor::new(), &req, stream);
-    println!("{:?}", resp);
+    info!("Websocket response: {:?}", resp);
     resp
 }
 
-
-
-#[actix_web::main]
+#[actix_web::main] 
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
+
+    info!("Starting server on 0.0.0.0:8888");
+
     HttpServer::new(|| {
         App::new()
-        .service(add_balances)
-        .service(ws_index)
+            .wrap(Logger::default())
+            .service(add_balances)
+            .service(ws_index)
     })
-        .bind(("0.0.0.0", 8888))?
-        .run()
-        .await
+    .bind(("0.0.0.0", 8888))?
+    .run()
+    .await
 }
-
--- mock-trading-server/src/websocket.rs
+++ mock-trading-server/src/websocket.rs
@@ -1,46 +1,12 @@
 use actix::prelude::*;
+use actix_web::cookie::time::error;
 use actix_web_actors::ws;
 use serde_json::{json, Value};
 use serde::{Deserialize, Serialize};
 use std::time::{SystemTime, UNIX_EPOCH};
 use uuid::Uuid;
 use rand::Rng;
-// Add websocket support for this
-//ExchangeClient is connecting via websocket to localhost:8888
-//
-//channel is active, starting websocket handshaking...
-//
-//Websocket client is connected
-//
-//Websocket client is authenticating for 3002
-//
-//Auth Request: 
-//
-//{"type":"AUTHENTICATE","api_token":"3002"}
-//
-//✓ Auth Acked Response:
-//
-//{type=AUTHENTICATED}
-//
-//Subscription Request:
-//
-//{"type":"SUBSCRIBE","channels":[{"name":"ORDERS"}]}
-//
-//✓ Subscriptions Acked
-//
-//{"channels":[{"account_id":3002,"name":"TRADING"}],"type":"SUBSCRIPTIONS","time":1698064922324}
-//
-//Limit Order Request:
-//
-//{"type":"CREATE_ORDER","order":{"instrument_code":"BTC_USDC","client_id":"4df7275e-6af9-4f2a-b620-7a9f6527a4f0","side":"BUY","type":"LIMIT","price":"1","amount":"1","time_in_force":"GOOD_TILL_CANCELLED"}}
-//
-//✓ Limit Order Acked:
-//
-//{"order_book_sequence":25532628695,"side":"SELL","uid":3002,"amount":"1","price":"1","instrument_code":"BTC_USDC","client_id":"4df7275e-6af9-4f2a-b620-7a9f6527a4f0","order_id":"2e81f80a-d6a0-48c0-bf17-4f63ba01d5a1","channel_name":"TRADING","type":"BOOKED","time":2002205959737}
-//
-//Cancel Order Request: 
-//
-//{"type":"CANCEL_ORDER","client_id":"4df7275e-6af9-4f2a-b620-7a9f6527a4f0","instrument_code":"BTC_USDC"}
+use log;
 
 // Message formats
 #[derive(Deserialize)]
@@ -106,14 +72,18 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebSocketActor {
         match msg {
             Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
             Ok(ws::Message::Text(text)) => {
-                println!("Received message: {}", text);
-
-                let payload: Value = serde_json::from_str(&text).unwrap(); 
-                let payload_type = payload["type"].as_str().unwrap();
+                debug!("Received message: {}", text);
+                let Ok(payload): Result<Value, _> = serde_json::from_str(&text) else {
+                    error!("Payload is invalid JSON: {}", text);
+                    return;
+                };
+                let Some(payload_type) = payload["type"].as_str() else {
+                    error!("Payload does not have a 'type' field: {}", payload);
+                    return;
+                };
 
                 match payload_type {
                     "AUTHENTICATE" => {
-                        // parse the json into AuthRequest
                         let auth_request: AuthRequest = serde_json::from_str(&text).unwrap();
                         self.user_id = Some(auth_request.api_token);
 
@@ -171,13 +141,13 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebSocketActor {
                         }).to_string());
                     }
                     _ => {
-                        // TODO: log an error
+                        error!("Ignoring unknown message type: {}", payload);
                     }
 
                 }
             },
             Ok(ws::Message::Close(reason)) => {
-                println!("Closing connection");
+                info!("Closing connection");
                 ctx.close(reason);
                 ctx.stop();
             },
