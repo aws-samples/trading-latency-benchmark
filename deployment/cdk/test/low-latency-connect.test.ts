fe84e58a2ff753830f51be0240408e13d5648f40
sercanka <sercanka@amazon.com>
2025-04-28 23:48:09 +0200

    Fix instance type parsing implementation
    - Remove leftover code fragments from previous implementation
    - Fix TypeScript compilation errors in all stack files
    - Update test file to use new stack names and properties
    - Ensure CDK synth works correctly with the new implementation

--- deployment/cdk/test/low-latency-connect.test.ts
+++ deployment/cdk/test/low-latency-connect.test.ts
@@ -1,11 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import {Template} from 'aws-cdk-lib/assertions';
-import * as LowLatencyConnect from '../lib/low-latency-connect-stack';
+import * as TradingBenchmark from '../lib/multi-az-stack';
 
 // Test: VPC is created
 test('VPC Created', () => {
     const app = new cdk.App();
-    const stack = new LowLatencyConnect.LowLatencyConnectStack(app, 'MyTestStack');
+    const stack = new TradingBenchmark.TradingBenchmarkMultiAzStack(app, 'MyTestStack');
     const template = Template.fromStack(stack);
 
     template.resourceCountIs('AWS::EC2::VPC', 1);
@@ -14,17 +14,16 @@ test('VPC Created', () => {
 // Test: Security Group is created with the correct ingress rule so that we can SSH into EC2s
 test('Security Group Created and Allows SSH', () => {
     const app = new cdk.App();
-    const stack = new LowLatencyConnect.LowLatencyConnectStack(app, 'MyTestStack');
+    const stack = new TradingBenchmark.TradingBenchmarkMultiAzStack(app, 'MyTestStack');
     const template = Template.fromStack(stack);
 
     const resources = template.findResources('AWS::EC2::SecurityGroup', {
         Properties: {
-            GroupDescription: 'Allow SSH access',
+            GroupDescription: 'Allow SSH access and internal traffic for trading benchmark',
             SecurityGroupEgress: [
                 {
-                    Description: 'Allow all outbound traffic by default',
-                    IpProtocol: '-1',
                     CidrIp: '0.0.0.0/0',
+                    IpProtocol: '-1',
                 },
             ],
         },
@@ -33,7 +32,7 @@ test('Security Group Created and Allows SSH', () => {
     expect(Object.keys(resources)).toHaveLength(1);
 
     //Make sure Security group allow inbound SSH connections
-    const securityGroup =  Object.entries(resources)[0][1]
+    const securityGroup = Object.entries(resources)[0][1];
     const ingressRule = securityGroup.Properties.SecurityGroupIngress[0];
     expect(ingressRule.CidrIp).toEqual('0.0.0.0/0');
     expect(ingressRule.FromPort).toEqual(22);
@@ -44,7 +43,7 @@ test('Security Group Created and Allows SSH', () => {
 // Test: 6 EC2 instances are created
 test('6 EC2 Instances Created', () => {
     const app = new cdk.App();
-    const stack = new LowLatencyConnect.LowLatencyConnectStack(app, 'MyTestStack');
+    const stack = new TradingBenchmark.TradingBenchmarkMultiAzStack(app, 'MyTestStack');
     const template = Template.fromStack(stack);
 
     const instances = Object.entries(template.findResources('AWS::EC2::Instance'));
@@ -52,10 +51,10 @@ test('6 EC2 Instances Created', () => {
     expect(instances.length).toEqual(6); // One instance per Availability Zone
 });
 
-// Test 7: One EC2 instance per AZ
+// Test: One EC2 instance per AZ
 test('There exist EC2 instance per AZ', () => {
     const app = new cdk.App();
-    const stack = new LowLatencyConnect.LowLatencyConnectStack(app, 'MyTestStack');
+    const stack = new TradingBenchmark.TradingBenchmarkMultiAzStack(app, 'MyTestStack');
     const template = Template.fromStack(stack);
 
     for (let az of ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"])
@@ -63,5 +62,4 @@ test('There exist EC2 instance per AZ', () => {
             AvailabilityZone: az
             }
         );
-
 });
